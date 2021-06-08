c59dec2bf69bb8934fcceb96856564a6ed251418
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-06-08 09:51:00 -0300

    adicionando correções no inicializador

--- src/main/java/com/mercadolivre/bootcamp/desafio/DAO/GenericDAO.java
+++ src/main/java/com/mercadolivre/bootcamp/desafio/DAO/GenericDAO.java
@@ -1,2 +1,7 @@
-package com.mercadolivre.bootcamp.desafio.DAO;public interface GenericDAO {
+package com.mercadolivre.bootcamp.desafio.DAO;
+
+import com.mercadolivre.bootcamp.desafio.models.BrandsModel;
+
+public interface GenericDAO <T>{
+    BrandsModel brand = new BrandsModel();
 }
